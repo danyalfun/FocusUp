f21eaaa223f8e980fe8a5df054488a01586ea83f
danyalfun <danyal.fun@gmail.com>
2026-04-08 22:57:35 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+const cors = require('cors');
+app.use(cors());
+app.use(express.static(__dirname + '/fe'));
+
+
+// מאפשר קריאת JSON מהבקשות POST
+app.use(bodyParser.json());
+
+// מאפשר CORS כדי שה-FE יוכל לשלוח בקשות
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
+// נקודת קצה ל-GET
+app.get("/get", (req, res) => {
+    console.log("GET request data:", req.query);
+    res.send("GET request התקבל!");
+});
+
+// נקודת קצה ל-POST
+app.post("/post", (req, res) => {
+    console.log("POST request data:", req.body);
+    res.send("POST request התקבל!");
+});
+
+// הפעלת השרת
+app.listen(port, () => {
+    console.log(`השרת רץ: http://localhost:${port}`);
+});
